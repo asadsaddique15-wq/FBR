8c516bff8d9646b315d2e6745739121f36934b94
asadsaddique15-wq <asad.saddique15@gmail.com>
2025-11-30 19:21:16 +0500

    fix: resolve remaining merge conflicts in app.module.ts and main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { VendorModule } from './vendor/vendor.module';
@@ -20,33 +19,5 @@ import { InvoiceModule } from './invoice/invoice.module';
     InvoiceModule,
   ],
   //providers: [MailerService],
-=======
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { VendorModule } from './modules/vendor/vendor.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { RegistryModule } from './modules/registry/registry.module';
-import { InvoiceModule } from './modules/invoice/invoice.module';
-import { FbrModule } from './modules/fbr/fbr.module';
-
-@Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        uri: config.get<string>('MONGO_URI'),
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }),
-    }),
-    VendorModule,
-    AuthModule,
-    RegistryModule,
-    InvoiceModule,
-    FbrModule,
-  ],
->>>>>>> 5d688854065ec36e355ff792028a8e7680bff78e
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
 async function bootstrap() {
   // create nest app
   const app = await NestFactory.create(AppModule);
@@ -22,34 +22,3 @@ async function bootstrap() {
   console.log(`Swagger UI available at http://localhost:3000/api`);
 }
 bootstrap();
-=======
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('v1');
-  app.useGlobalPipes(
-    new ValidationPipe({whitelist: true,transform: true,}),);
-  app.useGlobalInterceptors(new LoggingInterceptor());
-
-  const config = new DocumentBuilder()
-    .setTitle('FBR Digital Invoicing API')
-    .setDescription('Routers for vendors, auth, registry, and invoices')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .addTag('Vendors')
-    .addTag('Auth')
-    .addTag('Registry')
-    .addTag('Invoices')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-
-  await app.listen(process.env.PORT ?? 3000);
-}
-void bootstrap();
->>>>>>> 5d688854065ec36e355ff792028a8e7680bff78e
